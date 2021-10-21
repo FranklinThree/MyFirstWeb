4fb772c6c5f4c1b1705107cbac2670da12746997
FranklinThree <franklin3@hdu.edu.cn>
2021-10-21 13:15:11 +0800

    2021年10月21日13:15:03

--- QuestionnaireDatabase.sql
+++ QuestionnaireDatabase.sql
@@ -3,22 +3,37 @@ DROP TABLE IF EXISTS QuestionnaireDatabase;
 CREATE DATABASE QuestionnaireDatabase;
 USE QuestionnaireDatabase;
 
+DROP TABLE IF EXISTS QuestionnaireLists;
 DROP TABLE IF EXISTS QuestionLists;
-DROP TABLE IF EXISTS AUTO_TABLE_1;
+DROP TABLE IF EXISTS ChoiceLists;
+
 
 CREATE TABLE QuestionnaireLists(
-    UID BIGINT PRIMARY KEY,
+    Uid BIGINT PRIMARY KEY,
     QuestionnaireName VARCHAR(64) NOT NULL,
     QuestionnaireDescription VARCHAR(512) DEFAULT NULL
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-INSERT QuestionnaireLists SET UID = 1,QuestionnaireName = 'AUTO_TABLE_1',QuestionnaireDescription = 'xxx';
 
-CREATE TABLE AUTO_TABLE_1(
-    UID BIGINT PRIMARY KEY,
-    QuestionName VARCHAR(1024) NOT NULL,
-    QuestionDescription VARCHAR(256) DEFAULT NULL,
-    QuestionData VARCHAR(1024) DEFAULT NULL
+CREATE TABLE QuestionLists(
+    Uid                 BIGINT PRIMARY KEY,
+    Info                BIGINT        DEFAULT NULL,
+    QuestionName        VARCHAR(1024) NOT NULL,
+    QuestionDescription VARCHAR(256)  DEFAULT NULL,
+    QuestionAnswer      BIGINT DEFAULT NULL,
+    QuestionLink        BIGINT DEFAULT NULL
+)AUTO_INCREMENT = 1 ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE ChoiceLists(
+    Uid                 BIGINT PRIMARY KEY,
+    Info                BIGINT DEFAULT NULL,
+    ChoiceName          VARCHAR(1024) NOT NULL,
+    ChoiceDescription   VARCHAR(256) DEFAULT NULL,
+    ChoiceData          VARCHAR(1024) DEFAULT NULL,
+    ChoiceLink          BIGINT DEFAULT NULL
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-INSERT AUTO_TABLE_1 SET UID = 8888,QuestionName = 'Franklin3';
+
+INSERT QuestionnaireLists SET Uid = 11001,QuestionnaireName = '样卷1',QuestionnaireDescription = '仅供测试';
+INSERT QuestionLists SET Uid = 11001001,QuestionName = '您的谈过恋爱嘛？';
+INSERT ChoiceLists SET Uid = 11001001001,Info = ChoiceName = '是';
